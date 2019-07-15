0a91625a228aecbbebfa1f9b3a3d7936d5d6cef6
Mike Dimore <14313075+mikedimore@users.noreply.github.com>
2019-07-15 13:32:44 -0400

    Add files via upload

---
+++ PrizeOutDistribution/PrizeOut.framework/Headers/PrizeOut.h
@@ -0,0 +1,19 @@
+//
+//  PrizeOut.h
+//  PrizeOut
+//
+//  Created by Mike Dimore on 7/15/19.
+//  Copyright © 2019 Prize Out. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrizeOut.
+FOUNDATION_EXPORT double PrizeOutVersionNumber;
+
+//! Project version string for PrizeOut.
+FOUNDATION_EXPORT const unsigned char PrizeOutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrizeOut/PublicHeader.h>
+
+
